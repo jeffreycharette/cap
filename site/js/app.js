5e6655bc824bc51da5917a3525bc79ed8a8e88de
Jeff Charette <jeffrey@wearecharette.com>
2013-06-06 11:41:32 -0400

    hoverintent and stylz

--- site/js/app.js
+++ site/js/app.js
@@ -1,3 +1,10 @@
 $(function() {
-	console.log($('body').html());
+	$('nav ul li').hoverIntent(
+	  function () {
+	    $(this).find('ul').slideDown(100);
+	  },
+	  function () {
+	    $(this).find('ul').slideUp(400);
+	  }
+	);
 });
